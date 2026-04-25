c248c87f4b9f0134b54fa1f0c738986653e14785
Artem <dalyvinchi@gmail.com>
2026-04-25 23:23:42 +0300

    upd schema

--- src/schemas/transaction.py
+++ src/schemas/transaction.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class SendSchema(BaseModel):
-    user: str
-    amount: int
+    user: str 
+    amount: int = Field(gt=0)
